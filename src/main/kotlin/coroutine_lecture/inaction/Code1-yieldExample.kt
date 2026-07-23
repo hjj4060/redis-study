1f4e2917a27266a62258a490d0e2a3dfbcd54e3f
hyeonsu <hjj4060@goyoai.com>
2026-07-23 17:05:12 +0900

    feat: 코루틴 공부

---
+++ src/main/kotlin/coroutine_lecture/inaction/Code1-yieldExample.kt
@@ -0,0 +1,32 @@
+package coroutine_lecture.inaction
+
+import kotlinx.coroutines.*
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+fun now() = ZonedDateTime.now().toLocalTime().truncatedTo (ChronoUnit.MILLIS)
+fun log (msg:String) = println("${now()}: ${Thread.currentThread()}: $msg")
+
+fun yieldExample() {
+    runBlocking {
+        launch {
+            log("1")
+            yield()
+            log("3")
+            yield()
+            log("5")
+        }
+        println("after first launch")
+        launch {
+            log("2")
+            delay(1000L)
+            log("4")
+            delay(2000L)
+            log("6")
+        }
+        println("after second launch")
+    }
+}
+
+fun main() {
+    yieldExample()
+}
